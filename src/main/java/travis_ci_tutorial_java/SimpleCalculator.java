d6a1223029fb32eb4bfaa2cd76d0345f6c7c33ad
@jgonza32 <jgonza32@students.towson.edu>
2021-12-07 19:19:29 -0500

    all test implemented

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -7,4 +7,12 @@ public class SimpleCalculator {
 	public int sub(int a, int b) {
 		return a - b;
 	}
+	public int multi(int a, int b) {
+		return a * b;
+	}
+	public int div(int a, int b) {
+		if(b == 0)
+			throw new IllegalArgumentException("Not allowed to divide by 0")
+		return a / b;
+	}
 }
